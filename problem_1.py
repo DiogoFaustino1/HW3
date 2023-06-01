1361e6c4b0f4ff489b41092b2a827d4d71a1de8b
DiogoFaustino1 <diogovicentefaustino@tecnico.ulisboa.pt>
2023-06-01 23:27:00 +0100

    quase a acabar o 1.d)

--- problem_1.py
+++ problem_1.py
@@ -1,20 +1,9 @@
-import sympy as sym
 import openmdao.api as om
 
-#Derivatives of multivariable function
- 
-x , y = sym.symbols('x y')
-f = (1 - x)**2 + 100*(y - x**2)**2
 
-#Differentiating partially w.r.t x
-derivative_x = f.diff(x)
-derivative_y = f.diff(y)
-
-print('f\'(x)=', derivative_x, '\nf\'(y)=', derivative_y)
-
-class Rosenbrock(om.ExplicitComponent):
+class Paraboloid(om.ExplicitComponent):
     """
-    Evaluates the equation f(x,y) = (1 - x)**2 + 100*(y - x**2)**2
+    Evaluates the equation f(x,y) = (x-3)^2 + xy + (y+4)^2 - 3.
     """
 
     def setup(self):
@@ -29,9 +18,9 @@ class Rosenbrock(om.ExplicitComponent):
 
     def compute(self, inputs, outputs):
         """
-        f(x,y) = (1 - x)**2 + 100*(y - x**2)**2
+        f(x,y) = (x-3)^2 + xy + (y+4)^2 - 3
 
-        Minimum at: x = 1; y = 1
+        Minimum at: x = 6.6667; y = -7.3333
         """
         x = inputs['x']
         y = inputs['y']
@@ -42,24 +31,17 @@ class Rosenbrock(om.ExplicitComponent):
 if __name__ == "__main__":
 
     model = om.Group()
-    model.add_subsystem('comp', Rosenbrock(), promotes_inputs=['x', 'y'])
-    model.add_subsystem('const', om.ExecComp('g = x + y'))
-    # define the component whose output will be constrained 
+    model.add_subsystem('parab_comp', Paraboloid())
+
     prob = om.Problem(model)
     prob.setup()
 
-    prob.set_val('comp.x', 0.0)
-    prob.set_val('comp.y', 0.0)
+# build the model
+prob = om.Problem()
+prob.model.add_subsystem('parab', Paraboloid(), promotes_inputs=['x', 'y'])
 
-    prob.run_model()
-    print(prob['comp.f_xy'])
-
-    prob.set_val('comp.x', -1.0)
-    prob.set_val('comp.y', 1.0)
-
-    prob.run_model()
-    print(prob.get_val('comp.f_xy'))
-
+# define the component whose output will be constrained
+prob.model.add_subsystem('const', om.ExecComp('g = x + y'), promotes_inputs=['x', 'y'])
 
 # Design variables 'x' and 'y' span components, so we need to provide a common initial
 # value for them.
@@ -73,56 +55,43 @@ prob.driver.options['maxiter'] = 100000
 
 prob.model.add_design_var('x', lower=-2, upper=2)
 prob.model.add_design_var('y', lower=-2, upper=2)
-prob.model.add_objective('comp.f_xy')
-# to add the constraint to the model
-prob.model.add_constraint('const.g', upper=100)
+prob.model.add_objective('parab.f_xy')
 
 prob.setup()
-prob.run_driver()
+prob.run_driver();
 
 # minimum value
-print(prob.get_val('comp.f_xy'))
-# location of the minimum
-x_opt=prob.get_val('x')
-print("Unconstrained crllll", prob.get_val('x'))
-y_opt=prob.get_val('y')
-print(prob.get_val('y'))
-
-
-##############################################################################################
-
-# Design variables 'x' and 'y' span components, so we need to provide a common initial
-# value for them.
-prob.model.set_input_defaults('x', x_opt)
-prob.model.set_input_defaults('y', y_opt)
-
-# setup the optimization
-prob.driver = om.ScipyOptimizeDriver()
-prob.driver.options['optimizer'] = 'COBYLA'
-prob.driver.options['maxiter'] = 100000
-
-
-# Design variables 'x' and 'y' span components, so we need to provide a common initial
-# value for them.
-prob.model.set_input_defaults('x', -1.2)
-prob.model.set_input_defaults('y', 1.0)
-
-# setup the optimization
-prob.driver = om.ScipyOptimizeDriver()
-prob.driver.options['optimizer'] = 'SLSQP'
-prob.driver.options['maxiter'] = 100000
-
-#prob.model.add_design_var('x', lower=-2, upper=2)
-#prob.model.add_design_var('y', lower=-2, upper=2)
-#prob.model.add_objective('comp.f_xy')
-
-prob.setup()
-prob.run_driver()
-
-# minimum value
-print(prob.get_val('comp.f_xy'))
+print("Unconstrained optimization results:")
+print(prob.get_val('parab.f_xy'))
 # location of the minimum
 x_opt=prob.get_val('x')
 print(prob.get_val('x'))
 y_opt=prob.get_val('y')
+print(prob.get_val('y'))
+
+# to add the constraint to the model, starting from (-1.2,1.0)
+prob.model.add_constraint('const.g', upper=1)
+
+prob.setup()
+prob.run_driver();
+
+# minimum value
+print("Constrained optimization results starting from point (-1.2,1.0):")
+print(prob.get_val('parab.f_xy'))
+# location of the minimum
+print(prob.get_val('x'))
+print(prob.get_val('y'))
+
+#starting from the unconstrained optimal solution
+prob.model.set_input_defaults('x', x_opt)
+prob.model.set_input_defaults('y', y_opt)
+
+prob.setup()
+prob.run_driver();
+
+# minimum value
+print("Constrained optimization results starting from the unconstrained optimal solution:")
+print("Function value", prob.get_val('parab.f_xy'))
+# location of the minimum
+print(prob.get_val('x'))
 print(prob.get_val('y'))
